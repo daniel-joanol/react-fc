adf215db8731bf9d6f33d72065ca1b9e9d3528a3
elodin19 <joanoldaniel@gmail.com>
2022-01-09 15:55:54 +0100

    add formik and yup to login

--- components/forms/login.jsx
+++ components/forms/login.jsx
@@ -1,7 +1,20 @@
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router';
+import * as Yup from 'yup';
+import { Formik, Field, Form, ErrorMessage, isSubmitting} from 'formik';
 
 import Foot from '../pure/foot_login';
+
+const loginSchema = Yup.object().shape(
+    {
+        email: Yup.string()
+                .email('Invalid email format')
+                .required('Any email will login for now'),
+        password: Yup.string()
+                .required('Any password will do for now'),
+        check: Yup.bool()
+    }
+);
 
 const Login = () => {
 
@@ -15,46 +28,85 @@ const Login = () => {
         document.body.style.backgroundColor='#FFF';
     }, [])
 
-    return (
-        <div className="col left">
-            <form id="#main_form-login-page" action="database_form.html"  method="post">
-                <fieldset>
-                    
-                    <div>
-                        <p>OpenBootcamp<spam className="font_green bold"> | Alumnos</spam></p>
-                    </div>
-                    <div>
-                        <p className="nombre_campo">Email</p> 
-                        <input className="box-login-page" type="text" required id="password" name="email" placeholder="Introduce tu correo" />
-                    </div>
+    const initialCredentials = {
+        email: '',
+        password: '',
+        check: false
+    }
 
-                    <div>
-                        <p className="nombre_campo">Contraseña</p>
-                        <input className="box-login-page" type="password" name="password" id="password" required placeholder="Introduce tu contraseña" />
-                    </div>
-                    
-                    <div className="container2">
-                        <div className="row">
-                            <div className="col recuerdame">
-                                <input type="checkbox" name="check" id="check" /><label for="check">Recuerdame</label>
-                            </div>
-                            <div className="col olvidado"> 
-                                <p><a href="newpassword.html" target="_blank" className="font_green">He olvidado la contraseña</a></p>
-                            </div>
+    return (
+        <div className='col left'>
+            <Formik
+                initialValues = { initialCredentials }
+                validationSchema = { loginSchema }
+                onSubmit={async (values) => {
+                    await new Promise((r) => setTimeout(r, 100));
+                    await localStorage.setItem('credentials', values);
+                    navigateTo('/dashboard');
+                }}
+            >
+
+            {({
+                touched,
+                errors,
+                isSubmitting,
+                handleChange,
+                handleBlur }) =>(
+                    <div id='main_form-login-page'>
+                    <Form>
+                        <div>
+                            <p>OpenBootcamp<spam className="font_green bold"> | Alumnos</spam></p>
+                        </div>
+                        
+                        <div>
+                            <label htmlFor='email' className='nombre_campo'>Email</label>
+                            <Field id='email' className='box-login-page' type='email' name='email' placeholder='Introduce tu correo' />
+
+                            {
+                                errors.email && touched.email &&
+                                (
+                                    <ErrorMessage name='email' component='div'/>
+                                )
+                            }
+                        </div>
+                        
+                        <div>
+                            <label htmlFor='password' className='nombre_campo'>Password</label>
+                            <Field id='password' className='box-login-page' name='password' placeholder='Introduce tu contraseña'/>
+                        
+                            {
+                                (
+                                    <ErrorMessage name='password' component='div'/>
+                                )
+                            }
+                        
+                        </div>
+
+                        <div>
+                            <p></p>
+                            <input className="button" type="submit" value="Enviar" />
+                        </div>
+                        
+                        <div className="container2">
+                            <div className="row">
+                                <div className="col recuerdame">
+                                    <Field type="checkbox" name='check' id='check' /><label for="check">Recuerdame</label>
+                                </div>
+                                <div className="col olvidado"> 
+                                    <p><a href="newpassword.html" target="_blank" className="font_green">He olvidado la contraseña</a></p>
+                                </div>
                         </div>
                     </div>
-
-                    <div>
-                        <p></p>
-                        <input className="button" type="submit" value="Enviar" onClick={ () => navigateTo('/dashboard') } />
+                    </Form>
+                    <Foot />
                     </div>
                     
-                </fieldset>
-            </form>
+                    
+                )}
 
-            <Foot></Foot>
-          </div>
-    );
+            </Formik>
+        </div>
+    )
 }
 
 export default Login;
--- components/pure/foot_login.jsx
+++ components/pure/foot_login.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 const Foot = () => {
     return (
-        <div class="texto_pie">
-            <p>Copyright © 2021 Open Bootcamp SL, Imagina Group</p>
-            <p>Todos los derechos reservados.</p>
-            <p><a href="politica.html" target="_blank">Política de Privacidad</a></p>
+        <div className='d-flex justify-content-center'>
+            <div class="texto_pie">
+                <p>Copyright © 2021 Open Bootcamp SL, Imagina Group</p>
+                <p>Todos los derechos reservados.</p>
+                <p><a href="politica.html" target="_blank">Política de Privacidad</a></p>
+            </div>
         </div>
     );
 }
